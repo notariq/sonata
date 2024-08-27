92e0fb2ec6fe615d7c2ad17566c768a52ee05380
notariq <hauzanbakri@example.com>
2024-08-27 15:03:42 +0700

    image.commit fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,12 @@ import "./globals.css";
 import Sidebar from "@/components/sidebar/page";
 import Player from "@/components/player/page"
 import Footer from "@/components/footer";
+import { Open_Sans } from "next/font/google";
 
 import { AudioProvider } from "../contexts/audioContext";
 import { AuthProvider } from '../contexts/authContext';
+
+const sans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "SONATA",
@@ -19,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <AudioProvider><AuthProvider>
       <html lang="en">
-        <body className="bg-gradient-to-br from-gray-800 to-gray-600 text-white">
+        <body className={`${sans.className} bg-gradient-to-br from-gray-800 to-gray-600 text-white`}>
           <div className="flex">
             <Sidebar />
             <div className="px-16 pt-10 pb-[10%] h-full w-full relative">
